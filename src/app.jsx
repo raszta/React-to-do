99ff4ab4bdac20865582543d2fb053ef14fc5cc6
Imię i nazwiskoPiotr <raszta9999@wp.pl>
2018-09-14 12:38:57 +0200

    Adding alerts

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ListItem from './listItem.jsx';
 
-class App extends React.Component{
+class ToDoApp extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            toDo: [{ id: 1, activity: 'Learn some React'}, { id: 2, activity: 'Learn CSS'}, { id: 3, activity: 'Make love in the world!'},{id:4, activity: 'Make bed'}],
-            newToDo: ''          
+            toDo: [{ id: 1, activity: 'Learn some React'},
+            { id: 2, activity: 'Learn CSS'},
+            { id: 3, activity: 'Make love in the world!'},
+            {id:4, activity: 'Make bed'}],
+            newToDo: '',
+            editing: false,
+            editingIndex: null,
+            notification: null        
         }        
     }
 
@@ -18,13 +25,14 @@ class App extends React.Component{
 
     addToDo = (e) =>{
         const newDo = {
-            id: this.state.toDo[this.state.toDo.length-1].id +1,
+            id: this.state.toDo.length>0 ? this.state.toDo[this.state.toDo.length-1].id +1 : 1,
             activity: this.state.newToDo
         };
         this.setState({
             toDo: [...this.state.toDo, newDo],
             newToDo: ''
         });
+        this.msg("To do successfully added");
     }
 
     deleteToDo = (i) => {
@@ -33,31 +41,101 @@ class App extends React.Component{
         this.setState({
             toDo
         });
+        this.msg("To do successfully deleted");
+    }
+
+    editToDo = (i) =>{
+        const toDo = this.state.toDo[i];
+        this.setState({
+            editing: true,
+            newToDo: toDo.activity,
+            editingIndex: i
+        });
+    }
+
+    updateToDo = () => {        
+        const update = this.state.toDo;
+        update[this.state.editingIndex].activity = this.state.newToDo;
+        
+        this.setState({
+            editing: false,
+            editingIndex: null,
+            toDo: update,
+            newToDo: ''
+        });
+        this.msg("To do successfully updated");
+    }
+
+    removeAll = () =>{
+        this.setState({
+            toDo : []
+        });
+        this.msg("All successfully deleted");
+    }
+
+    removeDone = () => {
+
+    }
+
+    msg = (notification) =>{
+        this.setState({
+            notification
+        });
+
+        setTimeout(()=>{
+            this.setState({
+                notification: null
+            });
+        }, 2000);
     }
        
     render(){
-
         const list = this.state.toDo.map((el, i) => {
-            return <li className="list-group-item" key={el.id}>{el.activity}
-                <button className="btn btn-info ml-4 btn-sm">Done</button>
-                <button className="btn ml-4 btn-sm" onClick={() => { this.deleteToDo(i) }}><i className="fas fa-trash-alt"></i></button>  </li>
+            return <ListItem 
+            key={el.id}
+                el={el}
+                editToDo={()=>{this.editToDo(i)}}
+                deleteToDo={() => {this.deleteToDo(i)}} 
+            />
         });
 
         return <div className="container">
-            <h1 className="text-center my-5">To do Application</h1>
-            <div className="input-group mb-3">
+            <h1 className="text-center m-5 font-weight-bold">
+            To Do 
+            </h1>         
+            <div className="input-group my-3">
                 <input type="text" className="form-control" placeholder="Add new to do..." onChange={this.handleChange} value={this.state.newToDo} />
                 <div className="input-group-append">
-                    <button className="btn btn-outline-secondary" onClick={this.addToDo} id="button-addon2">Add </button>
+                    <button className="btn btn-outline-secondary text-light bg-dark" onClick={this.state.editing ?  this.updateToDo : this.addToDo} id="button-addon2">
+                    {this.state.editing ? "Edit to do" : "Add to do"}
+                     </button>
                     </div>
-            </div>            
-            <div className="mt-5 ">
+            </div>  
+            {
+                this.state.notification &&
+                <div className="alert alert-success">
+                    {this.state.notification}
+                </div>}             
+            {
+                !this.state.editing && 
+            <div className="mt-5 ">              
                 <ul className="list-group">
                     {list}
-                </ul>
-            </div> 
+                </ul>           
+                <button className="btn btn-info m-4 btn-sm" onClick={this.removeAll}>
+                Remove all
+                </button>         
+                <button className="btn btn-info m-4 btn-sm" onClick={this.removeDone}>
+                Remove done
+                </button>
+            </div> }
         </div>
     }
 }
-   
-ReactDOM.render(<App />, document.getElementById('app'));
+
+document.addEventListener('DOMContentLoaded', function () {
+    ReactDOM.render(
+        <ToDoApp />,
+        document.getElementById('app')
+    );
+});
---
+++ src/listItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+export default class ListItem extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return <li className="list-group-item">{this.props.el.activity}
+            <button className="btn btn-info ml-3 btn-sm" onClick={this.props.editToDo}>
+                Change
+                </button>
+            <button className="btn btn-info ml-3 btn-sm">
+                Done
+                </button>
+            <button className="btn ml-3 btn-sm" onClick={this.props.deleteToDo}>
+                <i className="fas fa-trash-alt"></i>
+            </button>
+        </li>
+    }
+}
